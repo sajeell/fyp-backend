45ec42a9ccea65fce9fd0b1db0e3a5baabd2785b
unknown <sajeel.ahmed@protonmail.com>
2022-01-03 02:24:35 +0500

    :tada: User MS Done

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { INestMicroservice } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const microservicesOptions: any = {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 8875,
+    },
+  };
+
+  const app: INestMicroservice = await NestFactory.createMicroservice(
+    AppModule,
+    microservicesOptions,
+  );
+
+  app.listen();
+}
+bootstrap();
