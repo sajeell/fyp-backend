bd0d4560f80957323dbfaa0fb7a740347c8002e9
unknown <sajeel.ahmed@protonmail.com>
2022-04-09 12:04:20 +0500

    :new: Add archived:Boolean Attribute In Order Document Mongoose

--- src/order/order.schema.ts
+++ src/order/order.schema.ts
@@ -5,32 +5,35 @@ export type OrderDocument = Order & Document
 
 @Schema()
 export class Order {
-  @Prop()
-  productID: string
+    @Prop()
+    productID: string
 
-  @Prop()
-  buyerID?: string
+    @Prop()
+    buyerID?: string
 
-  @Prop({ type: Number })
-  price: number
+    @Prop({ type: Number })
+    price: number
 
-  @Prop()
-  biddingID: string
+    @Prop()
+    biddingID: string
 
-  @Prop()
-  bargainingID: string
+    @Prop()
+    bargainingID: string
 
-  @Prop()
-  status?: string
+    @Prop()
+    status?: string
 
-  @Prop()
-  paymentStatus?: string
+    @Prop()
+    paymentStatus?: string
 
-  @Prop({ required: true })
-  createdAt: Date
+    @Prop({ required: true })
+    createdAt: Date
 
-  @Prop()
-  deletedAt?: Date
+    @Prop()
+    archived: Boolean
+
+    @Prop()
+    deletedAt?: Date
 }
 
 export const OrderSchema = SchemaFactory.createForClass(Order)
