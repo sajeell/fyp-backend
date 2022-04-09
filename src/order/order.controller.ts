138516a63622b7832fc64de5c6e553738d74859b
unknown <sajeel.ahmed@protonmail.com>
2022-04-09 12:07:21 +0500

    :new: Implement Controller Of Order

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { OrderService } from './order.service';
 
+@ApiBearerAuth()
+@ApiTags('Order')
 @Controller('order')
-export class OrderController {}
+export class OrderController {
+    constructor(private readonly service: OrderService) { }
+
+    @Get('/by/buyer/:id')
+    async getOrdersViaBuyerID(@Param('id') id: string) {
+        return await this.service.findAllViaBuyerID(id)
+    }
+
+    @Post('/create')
+    async createOrder(@Body() body: any) {
+        return await this.service.createBiddingOrder(body)
+    }
+}
